dfe625eb199a553d318c0438ac21aa96b5971379
Leslie McLeod <leslie.mcleod@ad.aie.edu>
2016-08-12 10:15:48 +0930

    no message

---
+++ New Unity Project/Assets/script test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scripttest : MonoBehaviour {
+
+     GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
